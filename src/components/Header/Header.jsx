acb77ec476dc03c6d281a29a1e48cbe33ec66f30
Philip Aguiar <PhilipAguiar@gmail.com>
2022-04-03 19:02:16 -0400

    added home link to logo in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import logo from "../../assets/close-around-v2.png"
+import logo from "../../assets/close-around-v2.png";
 
-function Header({clickHandler}) {
+function Header({ clickHandler }) {
   return (
     <div className="header">
-      <img className="header__logo" src={logo} alt="logo" />
-      <Link className="header__button" to= "/login">Login</Link>
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="logo" />
+      </Link>
+
+      <Link className="header__button" to="/login">
+        Login
+      </Link>
     </div>
   );
 }
