45d092720491e4a682306aa51519d20120ef3446
Philip Aguiar <PhilipAguiar@gmail.com>
2022-04-29 16:30:29 -0400

    refacted site to be deployed to firebase

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Middleware
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cors());
+app.options('*', cors())
+// Routes
+const ticketmasterRoute = require('./routes/ticketmaster');
+app.use('/ticketmaster', ticketmasterRoute);
+const eventRoute = require('./routes/events');
+app.use('/events', eventRoute);
+exports.app = functions.https.onRequest(app);
---
+++ functions/routes/events.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+const getEvents = () => JSON.parse(fs.readFileSync("./data/userEvents.json", { encoding: "utf8", flag: "r" }));
+
+const saveEvents = (events) => fs.writeFileSync("./data/userEvents.json", JSON.stringify(events));
+
+router.get("/", (req, res) => {
+  const events = getEvents();
+  if (events.length === 0) {
+    res.status(400).send("No events");
+    return;
+  }
+  res.status(200).json(events);
+});
+
+router.post("/", (req, res) => {
+  const events = getEvents();
+
+  events.push({
+    id: req.body.id,
+    lat: req.body.lat,
+    lng: req.body.lng,
+    icon: req.body.icon,
+    eventName: req.body.eventName,
+    eventDescription: req.body.eventDescription,
+    eventDate: req.body.eventDate,
+    eventLocation:req.body.eventLocation,
+    userSubmitted: req.body.userSubmitted,
+    userAvatar:req.body.userAvatar,
+    eventSize: req.body.eventSize,
+    usersInterested: req.body.usersInterested
+  });
+
+
+  saveEvents(events);
+  res.status(200).json(events);
+});
+
+router.post("/:id/edit", (req, res) => {
+  const events = getEvents();
+
+  let selectedEventExists =events.find(event=>event.id===req.params.id);
+  
+  if(!selectedEventExists){
+    res.status(500).json("No event with that id exists");
+  }
+
+  let editedEvent = {
+    id: req.body.id,
+    lat: req.body.lat,
+    lng: req.body.lng,
+    icon: req.body.icon,
+    eventName: req.body.eventName,
+    eventDescription: req.body.eventDescription,
+    eventDate: req.body.eventDate,
+    eventLocation:req.body.eventLocation,
+    userSubmitted: req.body.userSubmitted,
+    userAvatar:req.body.userAvatar,
+    eventSize: req.body.eventSize,
+    usersInterested: req.body.usersInterested
+  }
+ 
+  let editedEvents = events.map(event=>{
+    if (event.id === req.params.id) {
+      return editedEvent;
+    }
+    return event;
+  })
+
+  saveEvents(editedEvents);
+  res.status(200).json(editedEvents);
+});
+
+
+module.exports = router;
